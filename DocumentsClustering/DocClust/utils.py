765f7c371cfe74528c89b2443cfa2ff09c8f2cac
grg18120 <up1046903@upnet.gr>
2023-01-19 23:05:18 +0200

    Dataset code

--- DocumentsClustering/DocClust/utils.py
+++ DocumentsClustering/DocClust/utils.py
@@ -46,6 +46,51 @@ def tfidf(corpus):
 
     return [feature_names, doc_vectors]
  
+'''
+
+# ------------------------ ENGLISH DATASET ------------------------ #
+
+# newsgroup website http://qwone.com/~jason/20Newsgroups/
+
+# The sklearn.datasets.fetch_20newsgroups function is a data fetching / caching functions that 
+# downloads the data archive from the original 20 newsgroups website, extracts the archive contents in the ~/scikit_learn_data/20news_home folder and calls the sklearn.datasets.load_files on either the training or testing set folder, or both of them:
+
+# https://scikit-learn.org/stable/datasets/real_world.html#newsgroups-dataset
+
+from sklearn.datasets import fetch_20newsgroups
+
+newsgroups_train = fetch_20newsgroups(
+	subset = 'all', # "train" for the training set, "test" for the test set, "all" for both, with shuffled ordering.
+	categories = None, # If None (default), load all the categories, otherwise load categories from scratch(list)
+	shuffle = True, # shuffle data
+	random_state = 42, # static shuffling->int, total random->None
+	remove = (), # ('headers', 'footers', 'quotes') "headers" removes newsgroup headers, "footers" removes blocks at the ends of posts that look like signatures, and "quotes" removes lines that appear to be quoting another post.
+	download_if_missing = True, # If False, raise an IOError if the data is not locally available instead of trying to download the data from the source site.
+	return_X_y = False # True, returns (data.data, data.target) instead of the total object
+)
+
+true_labels = newsgroups_train.target 
+print(f"true labels: {newsgroups_train.target}")
+
+X = newsgroups_train.target.data
+print(f"data: {newsgroups_train.target.data}")
+
+print(f"file folder: {newsgroups_train.target.filenames}")
+
+print(f"full description of the dataset: {newsgroups_train.target.DESCR}")
+
+feature_names = newsgroups_train.target.target_names
+print(f"the names of target classes: {newsgroups_train.target.target_names}")
+
+#print(f"A tuple of two ndarray if return_X_y = True: {newsgroups_train}")
 
 
 
+
+# ------------------------ GREEK DATASET ------------------------ #
+
+from datasets import load_dataset
+
+dataset = load_dataset("greek_legal_code")
+
+'''
